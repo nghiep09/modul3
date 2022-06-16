bc88cea1be5b0e2b07301d204926aa49b50c288b
nghiep09 <nghiep1409@gmail.com>
2022-06-16 17:21:07 +0700

    a

---
+++ aaaaaaa.sql
@@ -0,0 +1,8 @@
+SELECT * FROM sys.student;
+drop table student;
+create table student(
+id int,
+name varchar(255),
+age int,
+address varchar(255)
+);
---
+++ bai_tap.sql
@@ -0,0 +1,11 @@
+create database student;
+create table class(
+id int,
+name varchar(255)
+);
+create table teacher(
+id int,
+name varchar(255),
+age int,
+country varchar(40)
+)
